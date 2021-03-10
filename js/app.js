bc9d1523081b1b9e65f1a6854a5d8c5b02b29765
Emilse <emilsearias@gmail.com>
2021-03-10 17:09:31 -0300

    agregaron cambios en leerfunkopop y mostramos las cards en la pagina principal

--- js/app.js
+++ js/app.js
@@ -11,9 +11,19 @@ function leerFunkopop() {
 
     // dibujar cada columna con los datos de los funkopop
     for (let i in listaFunkoPop) {
+      // cargar imagenes por defecto
+      let imagen = '';
+      if(listaFunkoPop[i].imagen ===''){
+        // cargar una imagen por defecto
+        imagen = 'thanos.png';
+      }else{
+        // usar la imagen cargada por el usuario
+        imagen = listaFunkoPop[i].imagen;
+      }
+
       let columna = `<div class="col-md-3 col-sm-6 my-2">
         <div class="card w-100 shadow">
-            <img src="img/productos/${listaFunkoPop[i].imagen}" class="card-img-top" alt="Funko ${listaFunkoPop[i].nombre}">
+            <img src="img/productos/${imagen}" class="card-img-top" alt="Funko ${listaFunkoPop[i].nombre}">
             <div class="card-body">
                 <h5 class="card-title">${listaFunkoPop[i].nombre}</h5>
                 <p class="card-text">${listaFunkoPop[i].descripcion}</p>
